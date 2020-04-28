b90ae81f0ad665f39c8b74b918a72a06c8698c26
vibhasantosh <bansalvibha81@gmail.com>
2020-04-28 15:42:15 -0400

    Update whole_food.java

--- src/ama_tab/whole_food.java
+++ src/ama_tab/whole_food.java
@@ -3,7 +3,8 @@ package ama_tab;
 public class whole_food {
 
 	public static void main(String[] args) {
-   System.out.println("This is test class");
+		// TODO Auto-generated method stub
+             System.out.println("This is test class...");
 	}
 
 }
